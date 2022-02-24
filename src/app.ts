68372d28bba8ed95c623cfd54627893bd443e6ec
umarmuhd <umarfarouqft@gmail.com>
2022-02-24 10:30:01 +0100

    updated cors

--- src/app.ts
+++ src/app.ts
@@ -13,9 +13,9 @@ app.use(express.json());
 
 app.use(deserializeUser);
 
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors({ origin: "*" }));
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
