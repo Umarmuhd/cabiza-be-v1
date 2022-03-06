76959468c5bd1e621c26e78656d70086d2eec64c
umarmuhd <umarfarouqft@gmail.com>
2022-03-06 15:29:17 +0100

    fix: change cors to global settings

--- src/app.ts
+++ src/app.ts
@@ -13,9 +13,12 @@ app.use(express.json());
 
 app.use(deserializeUser);
 
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors({ origin: "https://cabiza-fe-v1.vercel.app" }));
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://cabiza-fe-v1.vercel.app"
+  );
   res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
