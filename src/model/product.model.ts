d460d0d115d38c6810b6bc09619857beac30b680
umarmuhd <umarfarouqft@gmail.com>
2022-03-29 02:39:10 +0100

    change products categories

--- src/model/product.model.ts
+++ src/model/product.model.ts
@@ -14,9 +14,9 @@ class Affiliate {
 }
 
 enum CategoriesEnum {
-  "Classic",
-  "Pre Order",
-  "Membership",
+  "Education",
+  "Crafts & DIY",
+  "Health & Fitness",
 }
 enum CurrencyEnum {
   "Naira - ₦",
