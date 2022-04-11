467332953446cf4146c47345b126fa6360ee655b
umarmuhd <umarfarouqft@gmail.com>
2022-04-11 14:00:18 +0100

    fix

--- src/utils/braintree.ts
+++ src/utils/braintree.ts
@@ -1,15 +1,15 @@
-import braintree from "braintree";
+// import braintree from "braintree";
 
-const merchantId = process.env.BRAIN_TREE_MERCHANT_ID;
-const publicKey = process.env.BRAIN_TREE_PUBLIC_KEY;
-const privateKey = process.env.BRAIN_TREE_PRIVATE_KEY;
-const environment = braintree.Environment.Sandbox;
+// const merchantId = process.env.BRAIN_TREE_MERCHANT_ID;
+// const publicKey = process.env.BRAIN_TREE_PUBLIC_KEY;
+// const privateKey = process.env.BRAIN_TREE_PRIVATE_KEY;
+// const environment = braintree.Environment.Sandbox;
 
-const gateway = new braintree.BraintreeGateway({
-  environment,
-  merchantId,
-  publicKey,
-  privateKey,
-});
+// // const gateway = new braintree.BraintreeGateway({
+// //   // environment,
+// //   // merchantId,
+// //   // publicKey,
+// //   // privateKey,
+// // });
 
-export default gateway;
+// export default gateway;
