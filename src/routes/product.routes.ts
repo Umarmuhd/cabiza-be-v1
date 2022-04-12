bf6031c565331438b7fa83350a1d1dd22f057d80
umarmuhd <umarfarouqft@gmail.com>
2022-04-12 10:22:17 +0100

    creating a product

--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -13,18 +13,20 @@ import fileUpload from "../utils/file-upload";
 
 const router = express.Router();
 
-router.post(
-  "/new",
-  [
-    requireUser,
-    fileUpload.fields([
-      { name: "thumbnail", maxCount: 1 },
-      { name: "cover", maxCount: 1 },
-      { name: "file", maxCount: 1 },
-    ]),
-  ],
-  createNewProductHandler
-);
+router.post("/new", requireUser, createNewProductHandler);
+
+// router.post(
+//   "/new",
+//   [
+//     requireUser,
+//     fileUpload.fields([
+//       { name: "thumbnail", maxCount: 1 },
+//       { name: "cover", maxCount: 1 },
+//       { name: "file", maxCount: 1 },
+//     ]),
+//   ],
+//   createNewProductHandler
+// );
 
 router.get("/user/:user_id", getUserProductsHandler);
 
