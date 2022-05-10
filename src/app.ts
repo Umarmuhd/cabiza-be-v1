50fa994effc1a3a08db4530b06093b89a08ee44c
umarmuhd <umarfarouqft@gmail.com>
2022-05-11 00:27:58 +0100

    fix cors

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
-require("dotenv").config();
-import express from "express";
-import config from "config";
-import cors from "cors";
-import connectToDb from "./utils/connectToDb";
-import log from "./utils/logger";
-import router from "./routes";
-import deserializeUser from "./middleware/deserializeUser";
+require('dotenv').config();
+import express from 'express';
+import config from 'config';
+import cors from 'cors';
+import connectToDb from './utils/connectToDb';
+import log from './utils/logger';
+import router from './routes';
+import deserializeUser from './middleware/deserializeUser';
 
 const app = express();
 
@@ -13,21 +13,21 @@ app.use(express.json());
 
 app.use(deserializeUser);
 
-app.use(cors({ origin: "*" }));
+app.use(cors({ origin: 'https://cabiza.net' }));
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader('Access-Control-Allow-Origin', 'https://cabiza.net');
   res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   );
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
 
   next();
 });
 
 app.use(router);
 
-const port = config.get("port");
+const port = config.get('port');
 
 app.listen(port, () => {
   log.info(`App started at http://localhost:${port}`);
