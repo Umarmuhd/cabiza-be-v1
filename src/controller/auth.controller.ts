b4c5fb77c503edb1f0903eeba8f327b9840e695b
umarmuhd <umarfarouqft@gmail.com>
2022-05-11 10:38:23 +0100

    fix email verification url

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -1,24 +1,24 @@
-import { Request, Response } from "express";
-import { get, omit } from "lodash";
-import { addMinutes, isAfter } from "date-fns";
-import UserModel from "../model/user.model";
-import { CreateSessionInput } from "../schema/auth.schema";
+import { Request, Response } from 'express';
+import { get, omit } from 'lodash';
+import { addMinutes, isAfter } from 'date-fns';
+import UserModel from '../model/user.model';
+import { CreateSessionInput } from '../schema/auth.schema';
 import {
   findSessionById,
   signAccessToken,
   signRefreshToken,
-} from "../service/auth.service";
+} from '../service/auth.service';
 import {
   createUser,
   findUserByEmail,
   findUserById,
-} from "../service/user.service";
-import { verifyJwt } from "../utils/jwt";
-import { CreateUserInput, VerifyUserInput } from "../schema/user.schema";
-import { nanoid } from "nanoid";
-import WalletModel from "../model/wallet.model";
-import log from "../utils/logger";
-const Mailer = require("../utils/mailer");
+} from '../service/user.service';
+import { verifyJwt } from '../utils/jwt';
+import { CreateUserInput, VerifyUserInput } from '../schema/user.schema';
+import { nanoid } from 'nanoid';
+import WalletModel from '../model/wallet.model';
+import log from '../utils/logger';
+const Mailer = require('../utils/mailer');
 
 export async function signupUserHandler(
   req: Request<{}, {}, CreateUserInput>,
@@ -38,24 +38,22 @@ export async function signupUserHandler(
 
     await user.save();
 
-    const activationLink = `https://cabiza-fe-v1.vercel.app/auth/verify-email?token=${user.activation_code.token}&id=${user._id}`;
+    const activationLink = `https://cabiza.net/auth/verify-email?token=${user.activation_code.token}&id=${user._id}`;
 
-    await Mailer.send("confirm-account", user, {
+    await Mailer.send('confirm-account', user, {
       activationLink,
-      subject: "Welcome to Cabiza",
+      subject: 'Welcome to Cabiza',
     });
 
     return res
       .status(200)
-      .json({ message: "User created successfully", success: true });
+      .json({ message: 'User created successfully', success: true });
   } catch (e: any) {
     if (e.code === 11000) {
       return res
         .status(409)
-        .json({ success: false, message: "User already exists" });
+        .json({ success: false, message: 'User already exists' });
     }
-
-    console.log(e);
 
     return res.status(500).json({ success: false, message: e });
   }
@@ -73,19 +71,19 @@ export async function accountActivation(
   if (!user) {
     return res
       .status(400)
-      .json({ message: "could not verify user", success: false });
+      .json({ message: 'could not verify user', success: false });
   }
 
   if (user.verified) {
     return res
       .status(400)
-      .json({ message: "user is already verified", success: false });
+      .json({ message: 'user is already verified', success: false });
   }
 
   if (isAfter(new Date(), new Date(user.activation_code.expires_at!))) {
     return res
       .status(200)
-      .json({ success: false, message: "activation code expired ⌚" });
+      .json({ success: false, message: 'activation code expired ⌚' });
   }
 
   if (user.activation_code.token === activation_code) {
@@ -100,7 +98,7 @@ export async function accountActivation(
     const refreshToken = await signRefreshToken({ userId: user._id });
 
     return res.status(200).json({
-      message: "user is successfully verified",
+      message: 'user is successfully verified',
       success: true,
       accessToken,
       refreshToken,
@@ -109,14 +107,14 @@ export async function accountActivation(
 
   return res
     .status(500)
-    .json({ message: "could not verify user", success: false });
+    .json({ message: 'could not verify user', success: false });
 }
 
 export async function loginUserHandler(
   req: Request<{}, {}, CreateSessionInput>,
   res: Response
 ) {
-  const message = "Invalid email or password";
+  const message = 'Invalid email or password';
   const { email, password } = req.body;
 
   const user = await findUserByEmail(email);
@@ -128,7 +126,7 @@ export async function loginUserHandler(
   if (!user.verified) {
     return res
       .status(400)
-      .json({ success: false, message: "Please verify your email" });
+      .json({ success: false, message: 'Please verify your email' });
   }
 
   const isValid = await user.validatePassword(password);
@@ -143,34 +141,34 @@ export async function loginUserHandler(
 
   return res.status(200).json({
     success: true,
-    user: omit(user.toJSON(), ["password", "activation_code"]),
+    user: omit(user.toJSON(), ['password', 'activation_code']),
     accessToken,
     refreshToken,
   });
 }
 
 export async function refreshAccessTokenHandler(req: Request, res: Response) {
-  const refreshToken = get(req, "headers.x-refresh");
+  const refreshToken = get(req, 'headers.x-refresh');
 
   const decoded = verifyJwt<{ session: string }>(
     refreshToken,
-    "refreshTokenPublicKey"
+    'refreshTokenPublicKey'
   );
 
   if (!decoded) {
-    return res.status(401).send("Could not refresh access token");
+    return res.status(401).send('Could not refresh access token');
   }
 
   const session = await findSessionById(decoded.session);
 
   if (!session || !session.valid) {
-    return res.status(401).send("Could not refresh access token");
+    return res.status(401).send('Could not refresh access token');
   }
 
   const user = await findUserById(String(session.user));
 
   if (!user) {
-    return res.status(401).send("Could not refresh access token");
+    return res.status(401).send('Could not refresh access token');
   }
 
   const accessToken = signAccessToken(user);
@@ -184,28 +182,28 @@ export async function refreshAccessToken(req: Request, res: Response) {
   if (!refreshToken) {
     return res
       .status(400)
-      .json({ success: false, message: "invalid refresh token" });
+      .json({ success: false, message: 'invalid refresh token' });
   }
 
   const decoded = verifyJwt<{ session: string }>(
     refreshToken,
-    "refreshTokenPublicKey"
+    'refreshTokenPublicKey'
   );
 
   if (!decoded) {
-    return res.status(401).send("Could not refresh access token");
+    return res.status(401).send('Could not refresh access token');
   }
 
   const session = await findSessionById(decoded.session);
 
   if (!session || !session.valid) {
-    return res.status(401).send("Could not refresh access token");
+    return res.status(401).send('Could not refresh access token');
   }
 
   const user = await findUserById(String(session.user));
 
   if (!user) {
-    return res.status(401).send("Could not refresh access token");
+    return res.status(401).send('Could not refresh access token');
   }
 
   const accessToken = signAccessToken(user);
@@ -225,7 +223,7 @@ export async function forgetPasswordHandler(req: Request, res: Response) {
     if (!email) {
       return res
         .status(400)
-        .json({ success: false, message: "Please provide your email" });
+        .json({ success: false, message: 'Please provide your email' });
     }
 
     const user = await UserModel.findOne({ email });
@@ -233,16 +231,16 @@ export async function forgetPasswordHandler(req: Request, res: Response) {
     if (!user) {
       return res
         .status(404)
-        .json({ success: false, message: "Email not found please signup" });
+        .json({ success: false, message: 'Email not found please signup' });
     }
 
     const token = nanoid();
 
     user.password_reset = { token, expires_at: addMinutes(new Date(), 15) };
 
-    await Mailer.send("forgot-password", user, {
-      resetLink: `https://cabiza-fe-v1.vercel.app/auth/reset-password?token=${token}&email=${email}`,
-      subject: "forget your password",
+    await Mailer.send('forgot-password', user, {
+      resetLink: `https://cabiza.net/auth/reset-password?token=${token}&email=${email}`,
+      subject: 'forget your password',
     });
 
     await user.save();
@@ -264,7 +262,7 @@ export async function resetPasswordHandler(req: Request, res: Response) {
     if (!token) {
       return res.status(400).json({
         success: false,
-        message: "Please provide password reset token",
+        message: 'Please provide password reset token',
       });
     }
 
@@ -273,19 +271,19 @@ export async function resetPasswordHandler(req: Request, res: Response) {
     if (!user) {
       return res
         .status(400)
-        .json({ success: false, message: "Invalid password reset token" });
+        .json({ success: false, message: 'Invalid password reset token' });
     }
 
     if (isAfter(new Date(), new Date(user.password_reset.expires_at!))) {
       return res
         .status(202)
-        .json({ success: false, message: "Password reset token expired" });
+        .json({ success: false, message: 'Password reset token expired' });
     }
 
     if (user.password_reset.token !== token) {
       return res
         .status(400)
-        .json({ success: false, message: "Invalid password reset code" });
+        .json({ success: false, message: 'Invalid password reset code' });
     }
 
     user.password = password;
@@ -296,7 +294,7 @@ export async function resetPasswordHandler(req: Request, res: Response) {
 
     return res
       .status(200)
-      .json({ success: true, message: "User password reset successful" });
+      .json({ success: true, message: 'User password reset successful' });
   } catch (error: any) {
     log.error(error);
     return res.status(500).json({ success: false, message: error.message });
@@ -308,15 +306,15 @@ export async function updatePasswordHandler(req: Request, res: Response) {
 
   const { current_password, new_password } = req.body;
 
-  const user = await UserModel.findById(user_id).select("+password");
+  const user = await UserModel.findById(user_id).select('+password');
   if (!user) {
-    return res.status(400).json({ success: false, message: "user not found" });
+    return res.status(400).json({ success: false, message: 'user not found' });
   }
 
   if (current_password === new_password) {
     return res.status(400).json({
       success: false,
-      message: "current password should not be the same as new password",
+      message: 'current password should not be the same as new password',
     });
   }
 
@@ -325,10 +323,10 @@ export async function updatePasswordHandler(req: Request, res: Response) {
   if (!isPasswordMatched) {
     return res
       .status(400)
-      .json({ success: false, message: "current password is incorrect" });
+      .json({ success: false, message: 'current password is incorrect' });
   }
 
   user.password = new_password;
   await user.save();
-  res.status(200).json({ success: true, message: "password updated success!" });
+  res.status(200).json({ success: true, message: 'password updated success!' });
 }
